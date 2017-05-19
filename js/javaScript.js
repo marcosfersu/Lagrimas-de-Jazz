12948575f389f196b9a71775711c26b8e4fe3fa0
marcos.fers <marcos.fersu@gmail.com>
2017-05-18 22:21:02 -0300

    add audio and play bottom

--- js/javaScript.js
+++ js/javaScript.js
@@ -4,51 +4,61 @@
               anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
               menu: '#menu',
 
-			onLeave: function(index, nextIndex, direction){
-				var leavingSection = $(this);
-				var aboutEl = $('.textCenter');
-				var aboutElement = $('.vertical');
+				onLeave: function(index, nextIndex, direction){
+					var leavingSection = $(this);
+					var aboutEl = $('.textCenter');
+					var aboutElement = $('.vertical');
 
-				// after leaving section 1
-				if(index == 1 && direction =='down'){
-					aboutEl.removeClass("hidden");
+					// after leaving section 1
+					if(index == 1 && direction =='down'){
+						aboutEl.removeClass("hidden");
+					}
+
+					// after leaving section 2
+					if(index == 2 && direction =='up'){
+						aboutEl.addClass("hidden");
+					}
+
+					// after leaving section 3
+					if(index == 3 && direction =='up' && nextIndex == 1){
+						aboutEl.addClass("hidden");
+					}
+
+					// after leaving section 1
+					if(index == 1 && direction =='down' && nextIndex == 3){
+						aboutElement.removeClass("hidden1");
+					}
+
+					// after leaving section 2
+					if(index == 2 && direction =='up'){
+						aboutElement.addClass("hidden1");
+					}
+
+					// after leaving section 3
+					if(index == 3 && direction =='up' && nextIndex == 1){
+						aboutElement.addClass("hidden1");
+					}	
+
+				},
+
+				afterLoad: function(anchorLink, index){
+					var aboutElement = $('.vertical');
+
+					if(index == 2){
+						aboutElement.removeClass("hidden1");
+					}
+
 				}
 
-				// after leaving section 2
-				if(index == 2 && direction =='up'){
-					aboutEl.addClass("hidden");
-				}
+			});
 
-				// after leaving section 3
-				if(index == 3 && direction =='up' && nextIndex == 1){
-					aboutEl.addClass("hidden");
-				}
 
-				// after leaving section 1
-				if(index == 1 && direction =='down' && nextIndex == 3){
-					aboutElement.removeClass("hidden1");
-				}
 
-				// after leaving section 2
-				if(index == 2 && direction =='up'){
-					aboutElement.addClass("hidden1");
-				}
-
-				// after leaving section 3
-				if(index == 3 && direction =='up' && nextIndex == 1){
-					aboutElement.addClass("hidden1");
-				}	
-
-			},
-
-			afterLoad: function(anchorLink, index){
-				var aboutElement = $('.vertical');
-
-				if(index == 2){
-					aboutElement.removeClass("hidden1");
-				}
-
-			}
-
-		});
 	    });
+
+        $(document).ready(function() {
+
+			$(".play").click(function(){
+				$("audio")[0].play();
+			});
+		});
